b066ed0dd346305616b4600c71e7e1a84d3f0c85
Judy Natalia Fajardo Villafañe <yaniswil2712@gmail.com>
2024-04-12 19:22:37 -0500

    Inicial Commit

--- js/questions.js
+++ js/questions.js
@@ -1,14 +1,22 @@
 (function() {
-    const titleQuestions = [...document.querySelectorAll('.questions__title')];
-    console.log(titleQuestions);
-    titleQuestions.forEach(question => {
-        question.addEventListener('click', ()=> {
-            let height = 0;
-            let answer = question.nextElementSibling;
-            if (answer.clientHeight == 0 ) {
-                height = answer.scrollHeight;
+    const titleQuestions = document.querySelectorAll('.questions__title');
+    const questionContainers = document.querySelectorAll('.questions__padding');
+    
+    titleQuestions.forEach((question, index) => {
+        question.addEventListener('click', () => {
+            const container = questionContainers[index];
+            const computedStyles = window.getComputedStyle(container);
+            const currentOverflow = computedStyles.overflow;
+            container.style.height = '120px';
+            // Cambiar el valor del overflow basado en su estado actual
+            if (currentOverflow === "hidden") {
+               
+                container.style.overflow = "visible"; 
+                container.style.height = '290px';
+            } else {
+                container.style.overflow = "hidden";
+                container.style.height = '120px';
             }
-            answer.style.height =  `${height}px`;
-        })
-    })
+        });
+    });
 })();
